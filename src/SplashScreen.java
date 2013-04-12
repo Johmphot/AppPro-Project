f10bda8fc9a53bdca3c6199fcf3068b0e6ddb37c
Nijchaya Meeprasertskul <z.i.g.n@hotmail.com>
2013-04-12 14:45:58 +0700

    SplashScreen
    - splash screen 5seconds
    - random iceworld image

--- src/SplashScreen.java
+++ src/SplashScreen.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.awt.*;
 import javax.swing.*;
 
@@ -73,3 +74,63 @@ public class SplashScreen extends JWindow {
 		}
 	}
 }
+=======
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
+
+public class SplashScreen {
+  public static void main(String[] arg) {
+    JWindow jwin = new JWindow();
+    Image image= Toolkit.getDefaultToolkit().getImage("iceworld.jpg");
+    
+    /*InputStream is = getClass().getResourceAsStream("icewolrd.jpg");
+        image = ImageIO.read(is);*/
+    //ImageIcon splashIcon = new ImageIcon(image);
+    JLabel splashLabel = null;
+    splashLabel = new JLabel(new ImageIcon(image));
+    jwin.getContentPane()
+        .add(splashLabel);
+    jwin.setBounds(400, 200, 500, 300);
+    jwin.setVisible(true);
+
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    jwin.setVisible(false);
+    jwin.dispose();
+
+  }
+}
+
+/*import java.awt.Image;
+
+
+public class SplashScreen extends Thread{
+	int delay = 6000;
+	
+	public SplashScreen(){
+		Image s = new Image();
+	}
+	
+	public void run() {		
+		splash.setVisible(true);
+		this.sleep(delay);
+		splash.setVsiible(false);
+	} 
+	
+
+}*/
+>>>>>>> SplashScreen
